2337935c6d9fb2c1333da0acc2f9dd73209c459f
GollyTicker <golly.ticker@gmail.com>
2015-04-08 14:24:35 +0200

    fixed initHBQ message bug

--- server.erl
+++ server.erl
@@ -1,5 +1,6 @@
 
 -module(server).
+-import(werkzeug,[logging/2,get_config_value/2]).
 -export([start/0]).
 
 
@@ -19,25 +20,30 @@
 start() ->
   {ok, ConfigList} = file:consult("server.cfg"),
   PID = spawn( fun() -> State = initServer(ConfigList), loop(State) end),
-  {ok, ServerName} = werkzeug:get_config_value(servername, ConfigList),
+  {ok, ServerName} = get_config_value(servername, ConfigList),
   register(ServerName,PID),
   PID. 
 
 
 % initialisieren des CMEM, HBQ, DLQ, Logging und Erzeugen des Initialzustands
 initServer(ConfigList) -> 
+
   Datei = list_to_atom("log/Server@" ++ atom_to_list(node()) ++ ".log"),
-  werkzeug:logging(Datei, "Initalizing Server\n"),
-  {ok, RemTime} = werkzeug:get_config_value(clientlifetime, ConfigList),
+  logging(Datei, "Initalizing Server\n"),
+
+  {ok, RemTime} = get_config_value(clientlifetime, ConfigList),
   CMEM = cmem:initCMEM(RemTime,Datei),
-  {ok, HBQnode} = werkzeug:get_config_value(hbqnode,ConfigList),
-  {ok, HBQname} = werkzeug:get_config_value(hbqname,ConfigList),
+
+  {ok, HBQnode} = get_config_value(hbqnode,ConfigList),
+  {ok, HBQname} = get_config_value(hbqname,ConfigList),
   HBQservice = {HBQname,HBQnode},
-  HBQservice ! {self(), initHBQ},
+  logging(Datei, "Initializing HBQ - Address: " ++ werkzeug:to_String(HBQname)),
+  HBQservice ! {self(), {request,initHBQ}},
   receive
-    {reply, ok} -> werkzeug:logging(Datei, "Erhielt OK von HBQ\n")
+    {reply, ok} -> logging(Datei, "Erhielt OK von HBQ\n")
   end,
-  werkzeug:logging(Datei, "initialized Server\n"),
+  
+  logging(Datei, "initialized Server\n"),
   [CMEM, ConfigList, Datei]
   .
 
