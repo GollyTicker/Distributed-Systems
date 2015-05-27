ae17b6e1f3327d8f71b704bdb825a0b1a32cc5be
GollyTicker <golly.ticker@gmail.com>
2015-05-27 10:54:03 +0200

    added applicaiton main

---
+++ vsp3mw/src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ * Created by Swaneet on 27.05.2015.
+ */
+public class Main {
+//    ObjectBroker objBroker = ObjectBroker.init(host, port, false);
+//    NameService nameSvc = objBroker.getNameService();
+//    nameSvc.rebind((Object) myObject, “zumsel“);
+//
+//    objBroker.shutDown();
+
+    // Ebenso Client Code.
+}
