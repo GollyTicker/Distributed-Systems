b376b6b217050efbf090e92cd488f0fa45719938
GollyTicker <golly.ticker@gmail.com>
2015-05-31 11:31:25 +0200

    test for proxy todo

---
+++ vsp3mw/test/mware_lib/NameServiceProxyTest.java
@@ -0,0 +1,27 @@
+package mware_lib;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Swaneet on 31.05.2015.
+ */
+public class NameServiceProxyTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRebind() throws Exception {
+
+    }
+
+    @Test
+    public void testResolve() throws Exception {
+
+    }
+}
