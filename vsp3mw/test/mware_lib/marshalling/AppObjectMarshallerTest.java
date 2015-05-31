208c7df58e82f5b5679ef20fce07b9a95217591e
sacry- <nitsche25@gmail.com>
2015-05-31 11:32:19 +0200

    init marshaller

---
+++ vsp3mw/test/mware_lib/marshalling/AppObjectMarshallerTest.java
@@ -0,0 +1,32 @@
+package mware_lib.marshalling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sacry on 31/05/15.
+ */
+public class AppObjectMarshallerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMarshall() throws Exception {
+
+    }
+
+    @Test
+    public void testDemarshall() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
