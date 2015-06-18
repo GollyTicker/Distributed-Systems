9c2565b1f1f0b4f9d6d0f9d245505cc98723fdef
GollyTicker <golly.ticker@gmail.com>
2015-06-18 17:04:13 +0200

    added clock

--- src/clock.erl
+++ src/clock.erl
@@ -1,9 +1,31 @@
 -module(clock).
 -export([init/0]).
 
-init() -> loop(0).
+init(Offset) -> loop(Offset).
 
-loop(0) -> io:format("Fin");
-loop(N) -> 
-  io:format("Looping clock"),
-  loop(N - 1).
+-define(LOG,"log/clock.log").
+
+% PID ! {self(), getCurrentTimeMillis}
+% receive {timeMillis, MillisSince1970 } -> ... end
+
+% PID ! {updateOffset, Delta}
+% no response.
+
+loop(Offset) ->
+  receive
+  
+    {Sender, getCurrentTimeMillis} ->
+      {MegaSecs, Secs, MicroSecs} = now(),
+    	Millis = ((MegaSecs*1000000 + Secs)*1000000 + 1000*Offset + MicroSecs) div 1000.
+    	Sender ! {timeMillis, Millis},
+    	loop(Offset);
+    	
+  	{updateOffset, Delta} ->
+  	  NewOffset = Offset + Delta,
+  	  log(?LOG,clock,["Update offset (",Delta,"): ", NewOffset]),
+  	  loop(NewOffset)
+  
+  	Any -> 
+      log(?LOG, clock, ["Received unknown message: ", Any])
+    	
+  end.
