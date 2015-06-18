bbbf2963744099594182a703070c56d055819b35
sacry- <nitsche25@gmail.com>
2015-06-18 16:09:40 +0200

    removed java gitignores

---
+++ out/spawner.sh
@@ -0,0 +1,26 @@
+
+ls
+
+# out/
+# ├── datasource/
+# ├── spawner.sh
+# └── startStations.sh
+
+cd ..
+./build.sh
+cd out
+
+interfaceName=eth2
+mcastAddress=225.10.1.2
+receivePort=15010
+firstIndex=1
+lastIndex=2
+UTCoffsetMs=1
+
+stationClass=A
+./startStations.sh $interfaceName $mcastAddress $receivePort $firstIndex $lastIndex $stationClass $UTCoffsetMs
+
+stationClass=B
+./startStations.sh $interfaceName $mcastAddress $receivePort $firstIndex $lastIndex $stationClass $UTCoffsetMs
+
+# ./STDMAsniffer 225.10.1.2 15010 eth2 -adapt
