654fd7d8ee1107bb9788a9b6ec779cf4187d0dd7
sacry- <nitsche25@gmail.com>
2015-06-20 18:52:01 +0200

    wip spawner

--- out/spawner.sh
+++ out/spawner.sh
@@ -11,17 +11,13 @@ cd ..
 ./build.sh
 cd out
 
-interfaceName=eth2
+interfaceName=en0
 mcastAddress=225.10.1.2
 receivePort=15010
-firstIndex=1
-lastIndex=1
 UTCoffsetMs=1
 
-stationClass=A
-./startStations.sh $interfaceName $mcastAddress $receivePort $firstIndex $lastIndex $stationClass $UTCoffsetMs
+./startStations.sh $interfaceName $mcastAddress $receivePort 1 5 A $UTCoffsetMs
 
-stationClass=B
-# ./startStations.sh $interfaceName $mcastAddress $receivePort $firstIndex $lastIndex $stationClass $UTCoffsetMs
+# ./startStations.sh $interfaceName $mcastAddress $receivePort 2 4 B $UTCoffsetMs
 
-# ./STDMAsniffer 225.10.1.2 15010 eth2 -adapt
+# ./STDMAsniffer $mcastAddress $receivePort $interfaceName -adapt
