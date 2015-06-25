85f9dc9221512da1f1d09f8db9c6dcdd985377ea
GollyTicker <golly.ticker@gmail.com>
2015-06-25 21:53:27 +0200

    bug seems to be fixed. but slowness still causes some collisions

--- src/sender.erl
+++ src/sender.erl
@@ -1,12 +1,12 @@
 -module(sender).
--export([init/6]).
+-export([init/7]).
 
--import(utils,[log/3]).
+-import(utils,[log/4]).
 -import(datasource,[getNewData/1]).
 
 
-init(Config,Station,Source,Broker,Clock,Team) ->
-  log(sender, Team,["Sender start"]),
+init(Config,Station,Source,Broker,Clock,Team,DS) ->
+  log(DS,sender, Team,["Sender start"]),
 
   Con = udp_connect(Config),
   CurrNr = undefined,
@@ -15,12 +15,13 @@ init(Config,Station,Source,Broker,Clock,Team) ->
 
   FrameSent = 0,
   FrameNotSent = 0,
-  frameLoop(Con, CurrNr, Station, Source, Broker, Clock, Team,FrameSent,FrameNotSent).
+  CurrFrame = sync:frameNoByMillis(clock:getMillis(Clock)),
+  frameLoop(CurrFrame,Con, CurrNr, Station, Source, Broker, Clock, Team,FrameSent,FrameNotSent,DS).
 
 
 % loop invariant: Each iteration begins its execution at the beginning of a frame.
 
-frameLoop(Con, CurrNr, Station, Source, Broker, Clock, Team, FrameSent, FrameNotSent) -> 
+frameLoop(CurrFrame, Con, CurrNr, Station, Source, Broker, Clock, Team, FrameSent, FrameNotSent,DS) -> 
   Data = getNewData(Source),
   
   SentNextNr = case CurrNr of
@@ -32,47 +33,57 @@ frameLoop(Con, CurrNr, Station, Source, Broker, Clock, Team, FrameSent, FrameNot
         true ->
           sync:safeSleep(TimeToWait),
           ReserveNr = getNextFrameSlotNr(Broker),
-          sendMessage(Con, Clock, Broker, CurrNr, ReserveNr, Station, Data, Team);
+          sendMessage(Con, Clock, Broker, CurrNr, ReserveNr, Station, Data, Team,DS);
 
         false -> 
-          log(sender, Team, ["TimeToWait is negative, SentNextNr = undefined."]),
+          log(DS,sender, Team, ["TimeToWait is negative, SentNextNr = undefined."]),
           undefined
       end
   end,
   
-  ShouldBeFrame = sync:frameNoByMillis(clock:getMillis(Clock)),
   sync:waitToEndOfFrame(Clock), % at the end of the frame
-  checkPre(endOfFrame,Clock,Team,ShouldBeFrame),
-  % If nothing was sent, we need a new SlotNr for the next frame.
-  % If something was sent, check whether it didn't accidently collide. If so, then forget the reservation nr.
-  NothingSent = (SentNextNr =:= undefined),
-  OurSlotCollided = case CurrNr of undefined -> false; _ -> ourSlotCollided(Broker,CurrNr) end,
-  
-  CFrame = sync:frameNoByMillis(clock:getMillis(Clock)),
-  {NextNr2, NewFrameSent, NewFrameNotSent} =
-    case (NothingSent or OurSlotCollided) of
-      true  ->  Nr = getNextFrameSlotNr(Broker),
-                log(sender, Team, ["frame: ",CFrame, " - Failed. Trying my luck with ", Nr, " next frame"]),
-                {Nr, FrameSent, FrameNotSent + 1};
-      false ->  log(sender, Team, ["frame: ",CFrame, " - Sending successful. Reserved ", SentNextNr]),
-                {SentNextNr, FrameSent + 1, FrameNotSent}
+  PreCond = checkPre(endOfFrame,Clock,Team,CurrFrame,DS),
+  case PreCond of
+    ok -> 
+      % If nothing was sent, we need a new SlotNr for the next frame.
+      % If something was sent, check whether it didn't accidently collide. If so, then forget the reservation nr.
+      NothingSent = (SentNextNr =:= undefined),
+      OurSlotCollided = case CurrNr of undefined -> false; _ -> ourSlotCollided(Broker,CurrNr) end,
+      {NextNr2, NewFrameSent, NewFrameNotSent} =
+        case (NothingSent or OurSlotCollided) of
+          true  ->  Nr = getNextFrameSlotNr(Broker),
+                    log(DS,sender, Team, ["Failed Sending. Trying my luck with ", Nr, " next frame"]),
+                    {Nr, FrameSent, FrameNotSent + 1};
+          false ->  log(DS,sender, Team, ["Sending successful. Reserved ", SentNextNr]),
+                    {SentNextNr, FrameSent + 1, FrameNotSent}
+      end,
+      log(DS,sender, Team, ["Sent+NotSent=Total ",FrameSent, "+", FrameNotSent, "=", FrameSent+FrameNotSent]);
+    
+    nok -> % somehow we missed the end of the frame. be safe and reset to initial state.
+      {NextNr2,NewFrameSent,NewFrameNotSent} = {undefined, FrameSent, FrameNotSent + 1}
   end,
-
-  log(sender, Team, ["Sent+NotSent=Total ",FrameSent, "+", FrameNotSent, "=", FrameSent+FrameNotSent]),
+  sync:waitToNextFrame2(CurrFrame,Clock), % loop invariant
   
-  sync:waitToNextFrame(Clock), % loop invariant
+  NewFrame = sync:frameNoByMillis(clock:getMillis(Clock)),
   
-  frameLoop(Con, NextNr2, Station, Source, Broker, Clock, Team, NewFrameSent, NewFrameNotSent).
+  Diff = NewFrame - CurrFrame,
+  case Diff > 1 of
+    false -> % ok
+      frameLoop(NewFrame, Con, NextNr2, Station, Source, Broker, Clock, Team, NewFrameSent, NewFrameNotSent,DS);
+    true -> % waited too much. reset everything!
+      log(DS,sender, Team, ["Runtime too slow. Accidently skipped a frame. ", CurrFrame," -> ",NewFrame]),
+      frameLoop(NewFrame, Con, undefined, Station, Source, Broker, Clock, Team, NewFrameSent, NewFrameNotSent + 1,DS)
+  end.
 
 % CNr:       The slot nr we have to send the message in
 % ReserveNr: The slot nr to reserve in the next frame, if we succeed
-sendMessage(Con, Clock, Broker, CNr, ReserveNr, Station, Data, Team) ->
+sendMessage(Con, Clock, Broker, CNr, ReserveNr, Station, Data, Team,DS) ->
   SlotNr = sync:slotNoByMillis(clock:getMillis(Clock)),
 
   IsCorrectSlot = CNr == SlotNr,
   IsFree = isFree(Broker, CNr),
   
-  log(sender,Team, ["IsCorrectSlot: ",IsCorrectSlot," IsFree: ",IsFree," ",CNr," -> ",ReserveNr]),
+  log(DS,sender,Team, ["IsCorrectSlot: ",IsCorrectSlot," IsFree: ",IsFree," ",CNr," -> ",ReserveNr]),
   
   case (IsCorrectSlot and IsFree) of
     true -> 
@@ -83,11 +94,12 @@ sendMessage(Con, Clock, Broker, CNr, ReserveNr, Station, Data, Team) ->
     false -> undefined
   end.
 
-checkPre(endOfFrame,Clock,Team,ShouldBeFrame) ->
+checkPre(endOfFrame,Clock,Team,ShouldBeFrame,DS) ->
   {WasFrame,SlotNr,SlotTime} = FST = sync:fstByMillis(clock:getMillis(Clock)),
   case ((SlotNr =:= 25) and (SlotTime> 20) and (ShouldBeFrame =:= WasFrame)) of
     true -> ok;
-    false -> log(sender,Team,["Precondition violated: ShouldBeFrame ",ShouldBeFrame,", EndOfFrame failed with fst ", FST])
+    false -> log(DS,sender,Team,["Precondition violated: ShouldBeFrame ",ShouldBeFrame,", EndOfFrame failed with fst ", FST]),
+             nok
   end.
 
 ourSlotCollided(Broker,CurrNr) ->
